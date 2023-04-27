599720639eb27c1f1a9c2a5e371a21eb99439126
nikki02101 <122552460+nikki02101@users.noreply.github.com>
2023-04-27 18:40:55 +0200

    Add files via upload

---
+++ Feladatok/megoldas.js
@@ -0,0 +1,10 @@
+
+function DiakInfo(nev, csoport, típus) {
+    if (típus == true) {
+        console.log(nev + " Team" + csoport + " Junior Frontend");
+    }
+    else {
+        console.log(nev + " Team" + csoport + " Webprogramozó");
+    }
+}
+DiakInfo("Matus Nikolett", 11, true);
